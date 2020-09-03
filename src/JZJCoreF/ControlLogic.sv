0abef406ceab34e45874aea16bc3f47a35ed8b46
JZJisawesome <JZJisawesome123@hotmail.com>
2020-09-02 23:10:50 -0400

    Implement more of control logic

--- src/JZJCoreF/ControlLogic.sv
+++ src/JZJCoreF/ControlLogic.sv
@@ -10,20 +10,20 @@ module ControlLogic
 	
 	/* Control Lines */
 	//RegisterFile
-	output reg rdWriteEnable,
+	output rdWriteEnable,
 	//MemoryController
 	output MemoryMode_t memoryMode,
-	//RDInputChooser
-	output reg memoryOutputEnable,
-	output reg aluOutputEnable,
-	output reg immediateFormerOutputEnable,
-	output reg branchALUOutputEnable,
+	//RDInputChooser//todo make a struct or enum for this
+	output memoryOutputEnable,
+	output aluOutputEnable,
+	output immediateFormerOutputEnable,
+	output branchALUOutputEnable,
 	//ProgramCounter
-	output reg programCounterWriteEnable,
+	output programCounterWriteEnable,
 	//InstructionAddressMux
 	output InstructionAddressSource_t instructionAddressSource,
 	//ALU
-	output reg opImm,
+	output opImm,
 	//ImmediateFormer
 	output ImmediateFormerMode_t immediateFormerMode,
 	//BranchALU
@@ -39,6 +39,9 @@ module ControlLogic
 logic halt;//Next state should be state halt
 logic stop;//ecall/ebreak is signaling core to halt
 logic controlError;//Bad opcode or something similar
+assign halt = branchALUBadFunct3 | programCounterMisaligned | memoryUnalignedAccess | memoryBadFunct3 | stop | controlError | (opcode[1:0] != 2'b11);
+
+logic isTwoCycleInstruction;//Updated on posedge after state change to determine next state change
 
 //State Machine
 typedef enum {INITIAL_WAIT, INITIAL_FETCH, FETCH_EXECUTE, EXECUTE, HALT} State_t;
@@ -46,10 +49,55 @@ State_t currentState, nextState;
 
 /* State Machine Logic */
 
+//State Change
+always_ff @(negedge clock, posedge reset)
+begin
+	if (reset)
+		currentState <= INITIAL_WAIT;
+	else
+		currentState <= nextState;
+end
+
+//Decide nextState
+always_comb
+begin
+	if (halt)
+		nextState = HALT;//Initial entry into halt
+	else
+	begin
+		unique case (currentState)
+			INITIAL_WAIT: nextState = INITIAL_FETCH;
+			INITIAL_FETCH, FETCH_EXECUTE:
+			begin
+				if (isTwoCycleInstruction)
+					nextState = EXECUTE;//First cycle of 2 cycle instruction
+				else
+					nextState = FETCH_EXECUTE;//Instruction only takes 1 cycle
+			end
+			EXECUTE: nextState = FETCH_EXECUTE;//Move to the second cycle of a 2 cycle instruction
+			HALT: nextState = HALT;//Spin forever
+			default: nextState = HALT;//Invalid state occured; halt core
+		endcase
+	end
+end
+
+//Determine if instruction takes 2 cycles or not
+//Since opcode and funct3 update on the posedge, isTwoCycleInstruction also updates on the posedge (in time for the next state change)
+always_comb
+begin
+	//unique case (opcode) inside//Quartus Prime does not support case inside
+	unique casex (opcode)//Forced to do this instead
+		7'b00000xx: isTwoCycleInstruction = 1'b1;//load instructions
+		7'b01000xx: isTwoCycleInstruction = funct3 != 3'b010;//store instructions other than sw
+		default: isTwoCycleInstruction = 1'b0;//Either the instruction only takes 1 cycle, or this is a bad opcode, in which case it is handled by Control Line Logic section
+	endcase
+end
+
 /* Control Line Logic */
-
-/* Halt Logic */
-
-assign halt = branchALUBadFunct3 | programCounterMisaligned | memoryUnalignedAccess | memoryBadFunct3 | stop | controlError;
+//Also handles controlError
+always_comb
+begin
+	//todo
+end
 
 endmodule 
