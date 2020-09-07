841e77fd1f920b56af1424ab585e17aaa3d3f51a
JZJisawesome <JZJisawesome123@hotmail.com>
2020-09-06 22:25:11 -0400

    Fixed problem with verilator being unable to simulate generate statement in RegisterFile

--- src/JZJCoreF/RegisterFile.sv
+++ src/JZJCoreF/RegisterFile.sv
@@ -26,11 +26,10 @@ reg [31:0] registerFile [NUMBER_OF_REGISTERS:1];//x1 through x31
 assign rs1 = getRegister(registerFile, decodedAddresses.rs1Address);
 assign rs2 = getRegister(registerFile, decodedAddresses.rs2Address);
 
-generate
-begin
-	if (NUMBER_OF_REGISTERS == 32)
-		assign register31Output = registerFile[31];
-end
+generate if (NUMBER_OF_REGISTERS == 32)//RV32I
+	assign register31Output = registerFile[31];
+else if (NUMBER_OF_REGISTERS == 16)//x31 does not exist in RV32E
+	assign register31Output = 32'hxxxxxxxx;
 endgenerate
 
 /* Write Interface Logic */
@@ -57,76 +56,68 @@ end
 
 /* Read Multiplexer Function */
 
-generate
+function automatic logic [31:0] getRegister(input [31:0] registerFile [NUMBER_OF_REGISTERS:1], input [4:0] address);
 begin
-	if (NUMBER_OF_REGISTERS == 32)
-	begin
-		function automatic logic [31:0] getRegister(input [31:0] registerFile [NUMBER_OF_REGISTERS:1], input [4:0] address);
-		begin
-			unique case (address)//If I don't manually specify each register (eg. default: registerFile[address]), it creates 2 tiers of multiplexers and slows things down significantly
-				0: getRegister = 32'h00000000;//x0 is always 0
-				1: getRegister = registerFile[1];
-				2: getRegister = registerFile[2];
-				3: getRegister = registerFile[3];
-				4: getRegister = registerFile[4];
-				5: getRegister = registerFile[5];
-				6: getRegister = registerFile[6];
-				7: getRegister = registerFile[7];
-				8: getRegister = registerFile[8];
-				9: getRegister = registerFile[9];
-				10: getRegister = registerFile[10];
-				11: getRegister = registerFile[11];
-				12: getRegister = registerFile[12];
-				13: getRegister = registerFile[13];
-				14: getRegister = registerFile[14];
-				15: getRegister = registerFile[15];
-				16: getRegister = registerFile[16];
-				17: getRegister = registerFile[17];
-				18: getRegister = registerFile[18];
-				19: getRegister = registerFile[19];
-				20: getRegister = registerFile[20];
-				21: getRegister = registerFile[21];
-				22: getRegister = registerFile[22];
-				23: getRegister = registerFile[23];
-				24: getRegister = registerFile[24];
-				25: getRegister = registerFile[25];
-				26: getRegister = registerFile[26];
-				27: getRegister = registerFile[27];
-				28: getRegister = registerFile[28];
-				29: getRegister = registerFile[29];
-				30: getRegister = registerFile[30];
-				31: getRegister = registerFile[31];
-			endcase
-		end
-		endfunction
+	if (NUMBER_OF_REGISTERS == 32)//Elaboration-time check
+	begin//RV32I
+		unique case (address)//If I don't manually specify each register (eg. default: registerFile[address]), it creates 2 tiers of multiplexers and slows things down significantly
+			0: getRegister = 32'h00000000;//x0 is always 0
+			1: getRegister = registerFile[1];
+			2: getRegister = registerFile[2];
+			3: getRegister = registerFile[3];
+			4: getRegister = registerFile[4];
+			5: getRegister = registerFile[5];
+			6: getRegister = registerFile[6];
+			7: getRegister = registerFile[7];
+			8: getRegister = registerFile[8];
+			9: getRegister = registerFile[9];
+			10: getRegister = registerFile[10];
+			11: getRegister = registerFile[11];
+			12: getRegister = registerFile[12];
+			13: getRegister = registerFile[13];
+			14: getRegister = registerFile[14];
+			15: getRegister = registerFile[15];
+			16: getRegister = registerFile[16];
+			17: getRegister = registerFile[17];
+			18: getRegister = registerFile[18];
+			19: getRegister = registerFile[19];
+			20: getRegister = registerFile[20];
+			21: getRegister = registerFile[21];
+			22: getRegister = registerFile[22];
+			23: getRegister = registerFile[23];
+			24: getRegister = registerFile[24];
+			25: getRegister = registerFile[25];
+			26: getRegister = registerFile[26];
+			27: getRegister = registerFile[27];
+			28: getRegister = registerFile[28];
+			29: getRegister = registerFile[29];
+			30: getRegister = registerFile[30];
+			31: getRegister = registerFile[31];
+		endcase
 	end
-	else if (NUMBER_OF_REGISTERS == 16)
-	begin
-		function automatic logic [31:0] getRegister(input [31:0] registerFile [NUMBER_OF_REGISTERS:1], input [4:0] address);
-		begin
-			unique case (address)//If I don't manually specify each register (eg. default: registerFile[address]), it creates 2 tiers of multiplexers and slows things down significantly
-				0: getRegister = 32'h00000000;//x0 is always 0
-				1: getRegister = registerFile[1];
-				2: getRegister = registerFile[2];
-				3: getRegister = registerFile[3];
-				4: getRegister = registerFile[4];
-				5: getRegister = registerFile[5];
-				6: getRegister = registerFile[6];
-				7: getRegister = registerFile[7];
-				8: getRegister = registerFile[8];
-				9: getRegister = registerFile[9];
-				10: getRegister = registerFile[10];
-				11: getRegister = registerFile[11];
-				12: getRegister = registerFile[12];
-				13: getRegister = registerFile[13];
-				14: getRegister = registerFile[14];
-				15: getRegister = registerFile[15];
-				default: getRegister = 'x;//Invalid register field for RV32E
-			endcase
-		end
-		endfunction
+	else if (NUMBER_OF_REGISTERS == 16)//Elaboration-time check
+	begin//RV32E
+	unique case (address)//If I don't manually specify each register (eg. default: registerFile[address]), it creates 2 tiers of multiplexers and slows things down significantly
+			0: getRegister = 32'h00000000;//x0 is always 0
+			1: getRegister = registerFile[1];
+			2: getRegister = registerFile[2];
+			3: getRegister = registerFile[3];
+			4: getRegister = registerFile[4];
+			5: getRegister = registerFile[5];
+			6: getRegister = registerFile[6];
+			7: getRegister = registerFile[7];
+			8: getRegister = registerFile[8];
+			9: getRegister = registerFile[9];
+			10: getRegister = registerFile[10];
+			11: getRegister = registerFile[11];
+			12: getRegister = registerFile[12];
+			13: getRegister = registerFile[13];
+			14: getRegister = registerFile[14];
+			15: getRegister = registerFile[15];
+			default: getRegister = 'x;//Invalid register field for RV32E
+		endcase
 	end
 end
-endgenerate
+endfunction
 
 endmodule
